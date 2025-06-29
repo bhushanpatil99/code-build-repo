13aa522e14f925779a134307dc624855a334a1d6
bhushanpatil99 <bhushan3308@gmail.com>
2025-06-29 10:17:02 +0530

    Update setup_nginx.sh

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Grant execute permissions to this script (not necessary if already executed)
-chmod +x /home/ubuntu/scripts/setup_nginx.sh
-
 # Install Nginx
-apt update
-apt install -y nginx
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+apt-get install -y nginx
 
 # Start Nginx and enable it to run on boot
 systemctl start nginx
-systemctl enable nginx 
+systemctl enable nginx
 
 # Ensure Nginx is running
 if systemctl status nginx | grep "active (running)"; then
-    echo "Nginx is running."
+    echo "✅ Nginx is running."
 else
-    echo "Failed to start Nginx."
-    exit 1  # Exit with an error code if Nginx fails to start
+    echo "❌ Failed to start Nginx."
+    exit 1
 fi
